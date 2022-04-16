14e689ab09ec0853915f235b108de9eed4f418dd
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-04-16 22:03:56 +0300

    Finishing signup activity

--- app/src/main/java/com/savala/expressway/Register.java
+++ app/src/main/java/com/savala/expressway/Register.java
@@ -3,9 +3,15 @@ package com.savala.expressway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Register extends AppCompatActivity {
+
+    //widgets
+
+    private ImageView mBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,15 @@ public class Register extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
+
+        //init
+
+        mBack = findViewById(R.id.back);
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
