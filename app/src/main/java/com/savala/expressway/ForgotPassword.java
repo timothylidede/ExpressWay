d5235667221c0cc98e72bb643794ac2480c132b5
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-04-16 21:35:56 +0300

    Finishing forgotpass activity layout

--- app/src/main/java/com/savala/expressway/ForgotPassword.java
+++ app/src/main/java/com/savala/expressway/ForgotPassword.java
@@ -2,10 +2,19 @@ package com.savala.expressway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    //widgets
+    private ImageView mBack;
+
+    private TextView mReset, mPass, mInfo, mEmail, mForgotPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,29 @@ public class ForgotPassword extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_forgot_password);
+
+        //init
+        mBack = findViewById(R.id.back);
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        mReset = findViewById(R.id.reset_title);
+        mPass = findViewById(R.id.password_title);
+        mInfo = findViewById(R.id.info_title);
+        mEmail = findViewById(R.id.email_title);
+        mForgotPass = findViewById(R.id.forgot_pass);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Poppins-SemiBold.ttf");
+        Typeface tf2 = Typeface.createFromAsset(getAssets(), "fonts/Poppins-Regular.ttf");
+
+        mReset.setTypeface(tf);
+        mPass.setTypeface(tf);
+        mInfo.setTypeface(tf2);
+        mEmail.setTypeface(tf);
+        mForgotPass.setTypeface(tf2);
     }
 }
