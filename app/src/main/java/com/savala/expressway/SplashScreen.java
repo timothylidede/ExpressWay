a4e3141e957b78ce165b3ad4ceb7b881b6d36929
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-04-16 14:45:12 +0300

    Init widgets

--- app/src/main/java/com/savala/expressway/SplashScreen.java
+++ app/src/main/java/com/savala/expressway/SplashScreen.java
@@ -2,10 +2,15 @@ package com.savala.expressway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    //widgets
+    private TextView mExpress, mWay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,13 @@ public class SplashScreen extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
+
+        //init widgets
+        mExpress = findViewById(R.id.express_title);
+        mWay = findViewById(R.id.way_title);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Poppins-SemiBold.ttf");
+
+
     }
 }
