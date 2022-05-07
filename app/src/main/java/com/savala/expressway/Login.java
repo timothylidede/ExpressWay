fe709dd52365b44743f7c2234cf3711deb60728a
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-07 23:50:59 +0300

    Login finished

--- app/src/main/java/com/savala/expressway/Login.java
+++ app/src/main/java/com/savala/expressway/Login.java
@@ -1,5 +1,8 @@
 package com.savala.expressway;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,11 +11,21 @@ import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.method.PasswordTransformationMethod;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
@@ -21,9 +34,16 @@ public class Login extends AppCompatActivity {
 
     private TextView mSign, mIn, mInfo, mEmail, mPass, mForgotPass;
 
-    private EditText mEmailText, mPassText;
+    private EditText mLoginEmail, mLoginPassword;
+
+    private Button mLoginButton;
+
+    private ProgressBar mProgressBar;
 
     private ImageView mSeePass, mUnseePass;
+
+    //firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +68,11 @@ public class Login extends AppCompatActivity {
         mEmail = findViewById(R.id.email_title);
         mPass = findViewById(R.id.pass_title);
         mForgotPass = findViewById(R.id.forgot_pass);
+        mLoginButton = findViewById(R.id.login_button);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Poppins-SemiBold.ttf");
         Typeface tf2 = Typeface.createFromAsset(getAssets(), "fonts/Poppins-Regular.ttf");
@@ -71,8 +96,8 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        mEmailText = findViewById(R.id.email_text);
-        mPassText = findViewById(R.id.pass_text);
+        mLoginEmail = findViewById(R.id.email_text);
+        mLoginPassword = findViewById(R.id.pass_text);
 
         mSeePass = findViewById(R.id.see_pass);
         mUnseePass = findViewById(R.id.unsee_pass);
@@ -82,7 +107,7 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 mSeePass.setVisibility(View.INVISIBLE);
                 mUnseePass.setVisibility(View.VISIBLE);
-                mPassText.setTransformationMethod(null);
+                mLoginPassword.setTransformationMethod(null);
             }
         });
 
@@ -91,8 +116,57 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 mUnseePass.setVisibility(View.INVISIBLE);
                 mSeePass.setVisibility(View.VISIBLE);
-                mPassText.setTransformationMethod(new PasswordTransformationMethod());
+                mLoginPassword.setTransformationMethod(new PasswordTransformationMethod());
             }
         });
+    }
+
+    private void loginUser(){
+        String emailAddress = mLoginEmail.getText().toString().trim();
+        String password = mLoginPassword.getText().toString().trim();
+
+        if(emailAddress.isEmpty()){
+            mLoginEmail.setError("Key in email address");
+            mLoginEmail.requestFocus();
+            return;
+        }else if(password.isEmpty()){
+            mLoginPassword.setError("Key in password");
+            mLoginPassword.requestFocus();
+            return;
+        }else if(emailAddress.isEmpty() && password.isEmpty()){
+            Toast.makeText(Login.this, "Fill in all the fields", Toast.LENGTH_SHORT).show();
+        }else{
+            mProgressBar.setVisibility(View.VISIBLE);
+            mLoginButton.setVisibility(View.INVISIBLE);
+
+            mAuth.signInWithEmailAndPassword(emailAddress, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d(TAG, "onComplete: " + task.isSuccessful());
+
+                            if (!task.isSuccessful()){
+                                Toast.makeText(Login.this
+                                        , "Enter valid email address"
+                                        , Toast.LENGTH_SHORT).show();
+                                mProgressBar.setVisibility(View.GONE);
+                                mLoginButton.setVisibility(View.VISIBLE);
+                            }
+                            else{
+                                if(mAuth.getCurrentUser().isEmailVerified()){
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(Login.this, HomeActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    Toast.makeText(Login.this, "Verify your Email Address", Toast.LENGTH_LONG).show();
+                                }
+                                mLoginButton.setVisibility(View.VISIBLE);
+                                mProgressBar.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+        }
     }
 }
