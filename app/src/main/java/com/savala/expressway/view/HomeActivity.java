2369d7a5d23d93b0b9c79d8c2b74d22f1da0da7a
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-08 02:48:11 +0300

    Home Activity

--- app/src/main/java/com/savala/expressway/view/HomeActivity.java
+++ app/src/main/java/com/savala/expressway/view/HomeActivity.java
@@ -1,16 +1,68 @@
 package com.savala.expressway.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.savala.expressway.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        final View background = findViewById(R.id.am_background_view);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.am_view_pager);
+        HomePagerAdapter adapter = new HomePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        HomeBottomTabsView fomBottomTabsView = (HomeBottomTabsView) findViewById(R.id.am_home_tabs);
+        fomBottomTabsView.setUpWithViewPager(viewPager);
+
+        viewPager.setCurrentItem(1);
+
+        final int colorWhite = ContextCompat.getColor(this, R.color.white);
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+            {
+                if(position == 0){
+                    background.setBackgroundColor(colorWhite);
+                    background.setAlpha(1-positionOffset);
+                }else if(position == 1){
+                    background.setBackgroundColor(colorWhite);
+                    background.setAlpha(positionOffset);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
