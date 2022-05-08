77926c1ee59232cb7c2225a203880828cee06657
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-08 20:02:21 +0300

    Fixing bugs

--- app/src/main/java/com/savala/expressway/Logout.java
+++ app/src/main/java/com/savala/expressway/Logout.java
@@ -1,9 +1,13 @@
 package com.savala.expressway;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +15,7 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Logout extends Activity {
 
@@ -19,11 +24,14 @@ public class Logout extends Activity {
 
     //firbase
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
+
+        setupFirebaseAuth();
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -43,5 +51,43 @@ public class Logout extends Activity {
                 finish();
             }
         });
+    }
+
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user!=null){
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                }
+                else{
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+
+                    Log.d(TAG, "onAuthStateChanged: navigating back to sign activity");
+
+                    Intent intent = new Intent(getApplicationContext(), SignActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
