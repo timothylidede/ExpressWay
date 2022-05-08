bf7486ed7837f7fa3f739ead63b337cec3fed431
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-08 19:40:53 +0300

    Logout Theme Settings

--- app/src/main/java/com/savala/expressway/Logout.java
+++ app/src/main/java/com/savala/expressway/Logout.java
@@ -2,17 +2,15 @@ package com.savala.expressway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class Logout extends AppCompatActivity {
+public class Logout extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_logout);
     }
 }
