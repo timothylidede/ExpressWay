0310b246acaaeb7265a127815ceccc548ca4f8d3
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-08 15:37:56 +0300

    AccountFragment layout

--- app/src/main/java/com/savala/expressway/fragment/AccountFragment.java
+++ app/src/main/java/com/savala/expressway/fragment/AccountFragment.java
@@ -15,7 +15,7 @@ import com.savala.expressway.R;
 public class AccountFragment extends BaseFragment {
 
     //widgets
-    private TextView mExpress, mWay;
+    private TextView mExpress, mWay, mWhenTitle;
 
     public static AccountFragment create(){
         return new AccountFragment();
@@ -40,13 +40,21 @@ public class AccountFragment extends BaseFragment {
         top_layout1.setExitFadeDuration(3000);
         top_layout1.start();
 
+        RelativeLayout layout1 = (RelativeLayout) root.findViewById(R.id.layout1);
+        AnimationDrawable animationDrawable1 = (AnimationDrawable) layout1.getBackground();
+        animationDrawable1.setEnterFadeDuration(3000);
+        animationDrawable1.setExitFadeDuration(3000);
+        animationDrawable1.start();
+
         mExpress = root.findViewById(R.id.express_title);
         mWay = root.findViewById(R.id.way_title);
+        mWhenTitle = (TextView) root.findViewById(R.id.when_title);
 
         Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Poppins-SemiBold.ttf");
         Typeface tf2 = Typeface.createFromAsset(getContext().getAssets(), "fonts/Poppins-Regular.ttf");
 
         mExpress.setTypeface(tf);
         mWay.setTypeface(tf);
+        mWhenTitle.setTypeface(tf2);
     }
 }
