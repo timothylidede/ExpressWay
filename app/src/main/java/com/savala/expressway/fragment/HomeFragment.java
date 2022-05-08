56e782be2e7123b6c7ca057b212506d41e860e7f
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-08 07:31:28 +0300

    Home Fragment

--- app/src/main/java/com/savala/expressway/fragment/HomeFragment.java
+++ app/src/main/java/com/savala/expressway/fragment/HomeFragment.java
@@ -15,7 +15,8 @@ import com.savala.expressway.R;
 public class HomeFragment extends BaseFragment{
 
     //widgets
-    private TextView mSearchTitle;
+    private TextView mSearchTitle, mWhenTitle;
+    private TextView mExpress, mWay;
 
     public static HomeFragment create(){
         return new HomeFragment();
@@ -30,20 +31,32 @@ public class HomeFragment extends BaseFragment{
     public void inOnCreateView(View root, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         RelativeLayout layout = (RelativeLayout) root.findViewById(R.id.layout);
         AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
-        animationDrawable.setEnterFadeDuration(2000);
-        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
         animationDrawable.start();
 
         RelativeLayout layout1 = (RelativeLayout) root.findViewById(R.id.layout1);
         AnimationDrawable animationDrawable1 = (AnimationDrawable) layout1.getBackground();
-        animationDrawable1.setEnterFadeDuration(2000);
-        animationDrawable1.setExitFadeDuration(2000);
+        animationDrawable1.setEnterFadeDuration(3000);
+        animationDrawable1.setExitFadeDuration(3000);
         animationDrawable1.start();
 
+        RelativeLayout top_layout = (RelativeLayout) root.findViewById(R.id.top_layout);
+        AnimationDrawable top_layout1 = (AnimationDrawable) top_layout.getBackground();
+        top_layout1.setEnterFadeDuration(3000);
+        top_layout1.setExitFadeDuration(3000);
+        top_layout1.start();
+
         mSearchTitle = (TextView) root.findViewById(R.id.search_title);
+        mWhenTitle = (TextView) root.findViewById(R.id.when_title);
+        mExpress = root.findViewById(R.id.express_title);
+        mWay = root.findViewById(R.id.way_title);
 
         Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Poppins-SemiBold.ttf");
+        Typeface tf2 = Typeface.createFromAsset(getContext().getAssets(), "fonts/Poppins-Regular.ttf");
         mSearchTitle.setTypeface(tf);
-
+        mWhenTitle.setTypeface(tf2);
+        mExpress.setTypeface(tf);
+        mWay.setTypeface(tf);
     }
 }
