81f9eac950ac24bdb195e98e913a3d55d1c8925e
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-08 14:12:08 +0300

    Starting AccountFragment

--- app/src/main/java/com/savala/expressway/fragment/AccountFragment.java
+++ app/src/main/java/com/savala/expressway/fragment/AccountFragment.java
@@ -1,14 +1,22 @@
 package com.savala.expressway.fragment;
 
+import android.graphics.Typeface;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.savala.expressway.R;
 
 public class AccountFragment extends BaseFragment {
+
+    //widgets
+    private TextView mExpress, mWay;
+
     public static AccountFragment create(){
         return new AccountFragment();
     }
@@ -20,6 +28,25 @@ public class AccountFragment extends BaseFragment {
 
     @Override
     public void inOnCreateView(View root, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        RelativeLayout layout = (RelativeLayout) root.findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
 
+        RelativeLayout top_layout = (RelativeLayout) root.findViewById(R.id.top_layout);
+        AnimationDrawable top_layout1 = (AnimationDrawable) top_layout.getBackground();
+        top_layout1.setEnterFadeDuration(3000);
+        top_layout1.setExitFadeDuration(3000);
+        top_layout1.start();
+
+        mExpress = root.findViewById(R.id.express_title);
+        mWay = root.findViewById(R.id.way_title);
+
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Poppins-SemiBold.ttf");
+        Typeface tf2 = Typeface.createFromAsset(getContext().getAssets(), "fonts/Poppins-Regular.ttf");
+
+        mExpress.setTypeface(tf);
+        mWay.setTypeface(tf);
     }
 }
