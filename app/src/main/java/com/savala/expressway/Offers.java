3bcfcb42b2225709c0b1314183176c98d3ea3ff4
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-10 08:54:34 +0300

    Changing Themes

--- app/src/main/java/com/savala/expressway/Offers.java
+++ app/src/main/java/com/savala/expressway/Offers.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class VerifyNumber extends AppCompatActivity {
+public class Offers extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +13,6 @@ public class VerifyNumber extends AppCompatActivity {
         requestWindowFeature(getWindow().FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_verify_number);
+        setContentView(R.layout.activity_offers);
     }
 }
--- app/src/main/java/com/savala/expressway/fragment/AccountFragment.java
+++ app/src/main/java/com/savala/expressway/fragment/AccountFragment.java
@@ -114,7 +114,7 @@ public class AccountFragment extends BaseFragment {
         mOffers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
