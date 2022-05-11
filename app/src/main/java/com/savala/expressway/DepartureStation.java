e3b147c22c90ff373e8e1ca073e99fbc84a756d4
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-11 22:13:23 +0300

    RecyclerView deletion

--- app/src/main/java/com/savala/expressway/DepartureStation.java
+++ app/src/main/java/com/savala/expressway/DepartureStation.java
@@ -11,22 +11,17 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.savala.expressway.adapter.AdapterDepartureStation;
 import com.savala.expressway.fragment.AccountFragment;
-import com.savala.expressway.model.ModelTollStations;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,15 +34,6 @@ public class DepartureStation extends AppCompatActivity {
 
     private TextView mPickText;
 
-    private String station_uid;
-    private String station_name;
-
-    private DatabaseReference uRef;
-
-    RecyclerView recyclerView;
-    AdapterDepartureStation adapterDepartureStation;
-    List<ModelTollStations> stationList;
-
     public DepartureStation(){}
 
     @Override
@@ -57,9 +43,6 @@ public class DepartureStation extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_departure_station);
-
-        Intent intent = getIntent();
-        station_uid = intent.getStringExtra("station_uid");
 
         mBack = findViewById(R.id.back);
         mDone = findViewById(R.id.done);
@@ -88,19 +71,6 @@ public class DepartureStation extends AppCompatActivity {
         animationDrawable.setExitFadeDuration(3000);
         animationDrawable.start();
 
-        //init recyclerview
-        recyclerView = findViewById(R.id.departure_station_recycler_view);
-        recyclerView.setNestedScrollingEnabled(false);
-
-        //set its properties
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        //init userList
-        stationList = new ArrayList<>();
-
-        getStations();
-
         mBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -111,62 +81,7 @@ public class DepartureStation extends AppCompatActivity {
         mDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(DepartureStation.this, AccountFragment.class);
-                intent.putExtra("station_uid", station_uid);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        //get info of picked station
-        uRef = FirebaseDatabase.getInstance().getReference("TollStation");
-        Query dbQuery = uRef.orderByChild("station_id").equalTo(station_uid);
-        dbQuery.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot ds: snapshot.getChildren()){
-                    station_name = "" + ds.child("display_name").getValue();
-
-                    mPickText.setText(station_name);
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    private void getStations(){
-        //get path of database
-        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("TollStation");
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                stationList.clear();
-
-                for (DataSnapshot ds : snapshot.getChildren()) {
-                    //get data
-                    ModelTollStations station = ds.getValue(ModelTollStations.class);
-
-                    stationList.add(station);
-                }
-
-                //adapter
-                adapterDepartureStation = new AdapterDepartureStation(DepartureStation.this, stationList);
-
-                //refresh adapter
-                adapterDepartureStation.notifyDataSetChanged();
-
-                //set to recyclerView
-                recyclerView.setAdapter(adapterDepartureStation);
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+                
             }
         });
     }
