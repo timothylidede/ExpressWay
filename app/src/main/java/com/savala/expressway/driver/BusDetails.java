8ce1c877e3f0a977f16dddbdc464fcecc131eae3
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-06-30 00:56:32 +0300

    Back icon

--- app/src/main/java/com/savala/expressway/driver/BusDetails.java
+++ app/src/main/java/com/savala/expressway/driver/BusDetails.java
@@ -3,14 +3,26 @@ package com.savala.expressway.driver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.savala.expressway.R;
 
 public class BusDetails extends AppCompatActivity {
 
+    private ImageView mBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_details);
+
+        mBack = findViewById(R.id.back);
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
