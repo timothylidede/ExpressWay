8d47cd0d761f62f2f678ff053cb2ad4ba3a79e77
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-07-01 00:57:55 +0300

    top layout title

--- app/src/main/java/com/savala/expressway/driver/BusDetails.java
+++ app/src/main/java/com/savala/expressway/driver/BusDetails.java
@@ -2,15 +2,18 @@ package com.savala.expressway.driver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.savala.expressway.R;
 
 public class BusDetails extends AppCompatActivity {
 
     private ImageView mBack;
+    private TextView mExpress, mWay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,11 @@ public class BusDetails extends AppCompatActivity {
                 finish();
             }
         });
+        mExpress = findViewById(R.id.express_title);
+        mWay = findViewById(R.id.way_title);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Poppins-SemiBold.ttf");
+        Typeface tf2 = Typeface.createFromAsset(getAssets(), "fonts/Poppins-Regular.ttf");
+        mExpress.setTypeface(tf);
+        mWay.setTypeface(tf);
     }
 }
