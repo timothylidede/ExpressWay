9e0f999b2d1815a1317bf236469fc2df88106090
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-07-13 00:57:42 +0300

    PassengerInformation Class

---
+++ app/src/main/java/com/savala/expressway/PassengerInformation.java
@@ -0,0 +1,14 @@
+package com.savala.expressway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassengerInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_information);
+    }
+}
