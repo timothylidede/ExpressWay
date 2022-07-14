a770bce54702aba1e0ec7f5993e978672d41438b
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-07-14 23:35:06 +0300

    DropDown Maunufacturer done

--- app/src/main/java/com/savala/expressway/DriverInformation.java
+++ app/src/main/java/com/savala/expressway/DriverInformation.java
@@ -10,12 +10,21 @@ import android.view.WindowManager;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class DriverInformation extends AppCompatActivity{
-    private TextView mExpress, mWay, mManufacturer;
+    private TextView mExpress, mWay;
+
+    private String[] manufacturers = {"Mercedes-Benz AG", "Iveco S.P.A", "MAN SE", "AB Volvo",
+            "Scania AB", "EvoBus GmbH", "Temsa Europe NV", "Neoman Bus GmbH",
+            "Alexander Dennis Ltd", "Solaris Bus & Coach SA."};
+
+    private AutoCompleteTextView autoCompleteTextView;
+
+    ArrayAdapter<String> adapterItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +35,18 @@ public class DriverInformation extends AppCompatActivity{
         getSupportActionBar().hide();
         setContentView(R.layout.activity_driver_information);
 
+        autoCompleteTextView = findViewById(R.id.autoComplete_text);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.manufacturer_list, manufacturers);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                String manufacturer = parent.getItemAtPosition(position).toString();
+            }
+        });
+
         mExpress = findViewById(R.id.express_title);
         mWay = findViewById(R.id.way_title);
-        mManufacturer = (TextView) findViewById(R.id.manufacturer);
 
         RelativeLayout top_layout = (RelativeLayout) findViewById(R.id.top_layout);
         AnimationDrawable top_layout1 = (AnimationDrawable) top_layout.getBackground();
