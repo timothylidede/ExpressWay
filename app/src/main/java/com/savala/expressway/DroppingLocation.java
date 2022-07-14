b171705f106c9188366580674e33894b1385b04f
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-07-14 16:12:50 +0300

    DroppingLocation class

---
+++ app/src/main/java/com/savala/expressway/DroppingLocation.java
@@ -0,0 +1,14 @@
+package com.savala.expressway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DroppingLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dropping_location);
+    }
+}
