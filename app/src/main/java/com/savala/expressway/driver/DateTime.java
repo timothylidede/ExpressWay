91b91391564bcc21595793d347038d13e6e04757
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-07-16 13:08:49 +0300

    DateTime Activity

---
+++ app/src/main/java/com/savala/expressway/driver/DateTime.java
@@ -0,0 +1,14 @@
+package com.savala.expressway.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DateTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_time);
+    }
+}
