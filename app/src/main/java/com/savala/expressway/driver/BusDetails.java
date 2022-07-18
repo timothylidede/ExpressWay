990c66446850a4e02a3fe85cba3c1a135f7c9d12
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-07-18 04:52:44 +0300

    BusDetails logic done

--- app/src/main/java/com/savala/expressway/driver/BusDetails.java
+++ app/src/main/java/com/savala/expressway/driver/BusDetails.java
@@ -1,6 +1,8 @@
 package com.savala.expressway.driver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -8,6 +10,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.savala.expressway.R;
 
 public class BusDetails extends AppCompatActivity {
@@ -15,10 +22,28 @@ public class BusDetails extends AppCompatActivity {
     private ImageView mBack;
     private TextView mExpress, mWay;
 
+    private TextView mManufacturer, mNumberPlate, mYear, mNumberSeats, mRating;
+
+    private CardView mDoneButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_details);
+
+        mManufacturer = findViewById(R.id.manufacturer);
+        mNumberPlate = findViewById(R.id.number_plate);
+        mYear = findViewById(R.id.year_of_make);
+        mNumberSeats = findViewById(R.id.number_of_seats);
+        mRating = findViewById(R.id.rating);
+
+        mDoneButton = findViewById(R.id.done_button);
+        mDoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         mBack = findViewById(R.id.back);
         mBack.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +58,35 @@ public class BusDetails extends AppCompatActivity {
         Typeface tf2 = Typeface.createFromAsset(getAssets(), "fonts/Poppins-Regular.ttf");
         mExpress.setTypeface(tf);
         mWay.setTypeface(tf);
+
+        setBusDetails();
+    }
+
+    private void setBusDetails() {
+        FirebaseDatabase.getInstance().getReference("Driver")
+                .orderByChild("user_id").equalTo(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot ds:snapshot.getChildren()){
+                            String number_plate = "" + ds.child("number_plate").getValue();
+                            String bus_manufacturer = "" + ds.child("bus_manufacturer").getValue();
+                            String year = "" + ds.child("year").getValue();
+                            String number_of_seats = "" + ds.child("number_of_seats").getValue();
+                            String rating = "" + ds.child("rating").getValue();
+
+                            mManufacturer.setText(bus_manufacturer);
+                            mNumberPlate.setText(number_plate);
+                            mYear.setText(year);
+                            mNumberSeats.setText(number_of_seats);
+                            mRating.setText(rating);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 }
